cff7ad885733bcc3257e13ad253718663b2964c6
Phoenix <phoenix@Phoenixs-MacBook-Air.local>
2026-06-21 22:35:06 +0800

    Switch to BrowserRouter with 404 redirect for clean URLs

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './animations.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 )
